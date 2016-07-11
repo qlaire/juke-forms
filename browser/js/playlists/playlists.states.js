b84dc5fd7a9abd2da0449f2f88395200be7826d1
Claire Dranginis <cdranginis@gmail.com>
2016-07-11 15:41:56 -0400

    new playlist page set up

---
+++ browser/js/playlists/playlists.states.js
@@ -0,0 +1,8 @@
+'use strict';
+
+juke.config(function ($stateProvider) {
+  $stateProvider.state('newPlaylist', {
+    url: '/playlists/new',
+    templateUrl: '/js/playlists/templates/playlists.html'
+  });
+});
