14c5d5d9a6d9ab3d25616c7ab44db504e33ed087
Claire Dranginis <cdranginis@gmail.com>
2016-07-11 16:47:41 -0400

    implemented cached playlists

--- browser/js/playlists/playlists.factory.js
+++ browser/js/playlists/playlists.factory.js
@@ -1,19 +1,25 @@
 'use strict';
 
 juke.factory('PlaylistFactory', function($http) {
+
+  var cachedPlaylists = [];
+
   var PlaylistFactory = {};
 
   PlaylistFactory.create = function(data) {
     return $http.post('api/playlists', data)
     .then(function(response) {
-      return response.data;
+      var playlist = response.data;
+      cachedPlaylists.push(playlist);
+      return playlist;
     });
   };
 
   PlaylistFactory.fetchAll = function () {
     return $http.get('api/playlists')
     .then(function(response) {
-      return response.data;
+      angular.copy(response.data, cachedPlaylists);
+      return cachedPlaylists;
     });
   };
 
